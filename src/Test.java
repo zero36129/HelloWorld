58faa59c25acab2f2a3ef317c72050ced553af40
zero36129 <249851228@qq.com>
2017-07-31 17:56:11 +0800

    wrong commit.

--- src/Test.java
+++ src/Test.java
@@ -3,11 +3,6 @@ public class Test {
     
     public static void main(String[] args) {
         System.out.println("hello!");
-        System.out.println("world!");
-        System.out.println("back");
-        System.out.println("back2");
-        System.out.println("back3");
-        System.out.println("back4");
     }
 
 }
